3b58717aae3ea7d6e1e03624c12455aee4200364
universalezra <universalezra@gmail.com>
2025-07-01 22:50:20 +0700

    Update ezra.js

--- dashboard/ezra.js
+++ dashboard/ezra.js
@@ -145,7 +145,7 @@ class BioVAuth{
                 this.setCookie(this.securityConfig.loginStatusCookieName, 'true', this.securityConfig.loginCookieExpiryMinutes);
                 this.securitySystem.resetAttempts();
                 this.updateStatus('Authentication successful! Redirecting...','success');
-                window.location.href='dashboard';
+                window.location.href='dashboard/elvi.html';
             }else{
                 this.handleFailedLogin(responseData.message||'Invalid username or password.');
             }
@@ -164,7 +164,7 @@ class BioVAuth{
         this.securitySystem.resetAttempts();
         this.updateStatus('Authentication successful!','success');
         this.elements.errorMsg.textContent='';
-        window.location.href='dashboard';
+        window.location.href='dashboard/elvi.html';
     }
 
     handleFailedLogin(errorMessage){
